67d1f1d38be2f1691a914e8eb87b2c7f7b29b3e3
YF <robinycfang@gmail.com>
2020-05-10 11:58:37 +0800

    remove one unnecessary conditonal line

--- find_retained_introns.py
+++ find_retained_introns.py
@@ -3,10 +3,10 @@ import pandas as pd
 # get all introns of annotated transcripts
 anno_trans_introns = pd.read_csv('D:\\MCGDYY\\ont_project\\gtf_files\\anno_trans_introns.gtf', sep = '\t', chunksize = 10000, header= None,\
 						names = ['chromosome', 'source', 'type', 'start', 'end', 'none1', 'strand', 'none2', 'info'])
-anno_trans_introns = pd.concat(intron_gtf, ignore_index = True)
+anno_trans_introns = pd.concat(anno_trans_introns, ignore_index = True)
 
 # get all exons of novel transcripts
-novel_trans_exons = pd.read_csv('D:\\MCGDYY\\ont_project\\flair_out\\novel_trans_exons.gtf', header = None, sep = '\t',
+novel_trans_exons = pd.read_csv('D:\\MCGDYY\\ont_project\\gtf_files\\novel_trans_exons.gtf', header = None, sep = '\t',
 						names = ['chromosome', 'source', 'type', 'start', 'end', 'none1', 'strand', 'none2', 'info'])
 
 # check if intronic regions are included in the novel transcripts
@@ -26,20 +26,20 @@ for i in anno_trans_introns.index:
 			ex_start = tmp_df.loc[k, 'start']
 			ex_end = tmp_df.loc[k, 'end']
 
-		# full intron retaintion
-		if in_start >= ex_start and in_end <= ex_end:
-			retained_introns += str(in_chr) + '\t' + '*' + '\t' + 'intron' + '\t' + str(in_start) + '\t' + str(in_end) + '\t' 
-			+ 'IR' + '\t' + in_strand + '\t' + '*' + '\t' + tmp_df.loc[k, 'info'] + '\n'
+			# full intron retaintion
+			if in_start >= ex_start and in_end <= ex_end:
+				retained_introns += str(in_chr) + '\t' + '*' + '\t' + 'intron' + '\t' + str(in_start) + '\t' + str(in_end) + '\t' \
+				+ 'IR' + '\t' + in_strand + '\t' + '*' + '\t' + tmp_df.loc[k, 'info'] + '\n'
 
-		# alternative 5' splice site
-		if in_start <= ex_end and in_end >= ex_end:
-			retained_introns += str(in_chr) + '\t' + '*' + '\t' + 'intron' + '\t' + str(in_start) + '\t' + str(ex_end) + '\t' 
-			+ 'ALT5' + '\t' + in_strand + '\t' + '*' + '\t' + tmp_df.loc[k, 'info'] + '\n'
+			# alternative 5' splice site
+			if in_start <= ex_end and in_end >= ex_end:
+				retained_introns += str(in_chr) + '\t' + '*' + '\t' + 'intron' + '\t' + str(in_start) + '\t' + str(ex_end) + '\t' \
+				+ 'ALT5' + '\t' + in_strand + '\t' + '*' + '\t' + tmp_df.loc[k, 'info'] + '\n'
 
-		# alternative 3' splice site
-		if in_start <= ex_start and in_end >= ex_start:
-			retained_introns += str(in_chr) + '\t' + '*' + '\t' + 'intron' + '\t' + str(ex_start) + '\t' + str(in_end) + '\t' 
-			+ 'ALT3' + '\t' + in_strand + '\t' + '*' + + '\t' tmp_df.loc[k, 'info'] + '\n'
+			# alternative 3' splice site
+			if in_start <= ex_start and in_end >= ex_start:
+				retained_introns += str(in_chr) + '\t' + '*' + '\t' + 'intron' + '\t' + str(ex_start) + '\t' + str(in_end) + '\t' \
+				+ 'ALT3' + '\t' + in_strand + '\t' + '*' + '\t' + tmp_df.loc[k, 'info'] + '\n'
 
-with open('D:\\MCGDYY\\ont_project\\retained_introns.gtf', 'w') as w:
+with open('D:\\MCGDYY\\ont_project\\gtf_files\\retained_introns.gtf', 'w') as w:
 	w.write(retained_introns)
