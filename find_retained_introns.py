3d17e3c8dc1c4b5a6c31b9b1116016dfe2133ff8
YF <robinycfang@gmail.com>
2020-05-12 13:09:43 +0800

    Revert "use union introns of annotated genes instead"
    This reverts commit 81b5999e592219ca83abdc66a4930c416377fa41.

--- find_retained_introns.py
+++ find_retained_introns.py
@@ -1,23 +1,23 @@
 import pandas as pd
 
-# get all introns of annotated transcripts
-anno_trans_introns = pd.read_csv('D:\\MCGDYY\\ont_project\\gtf_files\\anno_trans_introns.gtf', 
+# get all union-introns of annotated genes
+anno_gene_union_introns = pd.read_csv('D:\\MCGDYY\\ont_project\\gtf_files\\anno_gene_union_introns.gtf', 
 	sep = '\t', chunksize = 10000, header= None, comment = '#',
 	names = ['chromosome', 'source', 'type', 'start', 'end', 'none1', 'strand', 'none2', 'info'])
-anno_trans_introns = pd.concat(anno_trans_introns, ignore_index = True)
+anno_gene_union_introns = pd.concat(anno_gene_union_introns, ignore_index = True)
 
 # get all exons of novel transcripts
-novel_trans_exons = pd.read_csv('D:\\MCGDYY\\ont_project\\gtf_files\\novel_trans_exons.gtf', 
+novel_trans_exons = pd.read_csv('D:\\MCGDYY\\ont_project\\gtf_files\\novel_mul_ex_trans_exons.gtf', 
 	header = None, sep = '\t', comment = '#',
 	names = ['chromosome', 'source', 'type', 'start', 'end', 'none1', 'strand', 'none2', 'info'])
 
-# check if intronic regions are included in the novel transcripts
+# check if union intronic regions of annotated genes are included in the novel transcripts
 retained_introns = ''
-for i in anno_trans_introns.index:
-	in_chr = anno_trans_introns.loc[i, 'chromosome']
-	in_start = anno_trans_introns.loc[i, 'start']
-	in_end = anno_trans_introns.loc[i, 'end']
-	in_strand = anno_trans_introns.loc[i, 'strand']
+for i in anno_gene_union_introns.index:
+	in_chr = anno_gene_union_introns.loc[i, 'chromosome']
+	in_start = anno_gene_union_introns.loc[i, 'start']
+	in_end = anno_gene_union_introns.loc[i, 'end']
+	in_strand = anno_gene_union_introns.loc[i, 'strand']
 
 	# select specific chromosome and nearby sites to reduce iteration and speed up the script
 	tmp_df = novel_trans_exons[novel_trans_exons['chromosome'] == str(in_chr)]
