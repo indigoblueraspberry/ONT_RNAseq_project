f237b05a7da620f9d6ab5f168f1efaffa89da3cc
Yuanchang Fang <robinycfang@gmail.com>
2020-08-21 15:43:55 +0800

    batch update

---
+++ analyze_novel_lncRNA/bp_ex_seq_DE_prog_nov_trans.py
@@ -0,0 +1,54 @@
+import pandas as pd
+
+# create tables for box plots of log2FC of DE and clinically relevant novel transcripts 
+
+exp_df = pd.read_csv('D:\\MCGDYY\\ont_project\\quantification\\t_exp_median_DEonly.csv')
+DE_prog_novel_trans_list = []
+
+def create_bp_input(prog):	# prog = os or rfs
+
+	sig_prog_df = pd.read_csv('D:\\MCGDYY\\ont_project\\prognosis\\sig_res_' + prog + '.csv')
+	box_plot_df = 'order	transcript	log2FC	status' + '\n'
+
+	for i in sig_prog_df.index:
+		trans_name = sig_prog_df.loc[i, 'transcript']
+		trans_name = trans_name.replace('_', '-')
+		trans_name = trans_name.replace('trans-', '')
+		trans_name = trans_name.replace('-status', '')
+
+		avg_log2FC = exp_df[trans_name + '_value'].mean()
+		if avg_log2FC > 0.58:
+			status = 'up'
+		if avg_log2FC < -0.58:
+			status = 'down'
+		order = sig_prog_df.loc[i, 'order']
+
+		for k in exp_df[trans_name + '_value']:
+			box_plot_df += str(order) + '\t' + trans_name + '\t' + str(k) + '\t' + status + '\n'
+		if trans_name not in DE_prog_novel_trans_list:
+			DE_prog_novel_trans_list.append(trans_name)
+
+	# with open('D:\\MCGDYY\\ont_project\\prognosis\\FC_boxplot_' + prog + '.txt', 'w') as w:
+	# 	w.write(box_plot_df)
+
+create_bp_input('os')
+create_bp_input('rfs')
+
+
+# extract sequence of DE and clinically relevant novel transcripts
+
+extracted_seq = ''
+for i in DE_prog_novel_trans_list:
+	with open('D:\\MCGDYY\\ont_project\\lncRNA\\lncRNA_pred\\novel_uniq_RI_real_seq_200bp.fa', 'r') as master_seq:
+		line = master_seq.readline()
+		while line:
+			if line.startswith('>'):
+				if i in line:
+					extracted_seq += line
+					line = master_seq.readline()
+					extracted_seq += line
+					break
+			line = master_seq.readline()
+
+with open('D:\\MCGDYY\\ont_project\\lncRNA\\lncRNA_pred\\DE_clinical_novel_trans_real_seq.fa', 'w') as w:
+	w.write(extracted_seq)
---
+++ quantify_novel_trans/DE_novel_trans.py
@@ -0,0 +1,29 @@
+# differentially expressed analysis on novel transcripts
+
+import pandas as pd
+from scipy import stats
+from statsmodels.stats.multitest import multipletests
+
+exp_df = pd.read_csv('D:\\MCGDYY\\ont_project\\quantification\\t_exp_median.csv')
+sum_df = pd.DataFrame(columns = ['log2FC', 'wilcoxon_p', 'adj_p'])
+
+for i in exp_df.columns:
+	if '_value' in i:
+		trans_name = i.split('_')[0]
+		log2FC = exp_df[i].mean()
+		sum_df.loc[trans_name, 'log2FC'] = log2FC
+		wc_p = stats.wilcoxon(exp_df[i])[1]
+		sum_df.loc[trans_name, 'wilcoxon_p'] = wc_p
+# multiple comparison correction
+fdr_bh = multipletests(sum_df['wilcoxon_p'], method='fdr_bh')[1]
+sum_df['adj_p'] = fdr_bh
+DE_trans = sum_df[(abs(sum_df['log2FC']) > 0.58) & (sum_df['adj_p'] < 0.05)]
+DE_list = DE_trans.index
+
+DE_df = exp_df
+for i in exp_df.columns[15:]:
+	trans_name = i.split('_')[0]
+	if trans_name not in DE_list:
+		DE_df = DE_df.drop(columns = [i])
+sum_df.to_csv('D:\\MCGDYY\\ont_project\\quantification\\sum_DE_all_novel.csv')
+DE_df.to_csv('D:\\MCGDYY\\ont_project\\quantification\\t_exp_median_DEonly.csv', index = False)
