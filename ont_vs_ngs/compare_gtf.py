09a3a28cb4864ee00fd98896a20f882e71ed2de0
Yuanchang Fang <robinycfang@gmail.com>
2021-04-11 23:42:21 +0800

    Update compare_gtf.py

--- ont_vs_ngs/compare_gtf.py
+++ ont_vs_ngs/compare_gtf.py
@@ -1,10 +1,35 @@
-# find overlapping and unique transcripts from two gtf files
+# find overlapping and unique transcripts from two gtf files generated from gffcompare
 # remember to sort gtf files based on chromosome and coordinates
 
+import pandas as pd
 import  re
 
 
-def convert_gtf(gtf_path):
+# # convert flair gtf output to a standard gtf format
+
+# flair_gtf = pd.read_csv('D:\\MCGDYY\\ont_project\\flair_out\\flair.collapse.isoforms.stringent.gtf', sep = '\t', header = None)
+# flair_sort = flair_gtf.sort_values(by = [0, 3])
+# flair_list = []
+# for i in flair_sort.index:
+# 	info = flair_sort.loc[i, 8]
+# 	trans = re.findall(r'transcript_id "(.*?)";', info)[0]
+# 	if trans not in flair_list:
+# 		flair_list.append(trans)
+
+# flair_compiled = ''
+# for i in flair_list:
+# 	sub = flair_gtf[flair_gtf[8].str.contains(i)].sort_values(by = 3)
+# 	flair_compiled += str(sub.iloc[0, 0]) + '\tFLAIR\ttranscript\t' + str(sub.iloc[0, 3]) + '\t' + str(sub.iloc[len(sub) - 1, 4]) + '\t' + i + '\n'
+# 	for k in sub.index:
+# 		line = '\t'.join(str(j) for j in sub.loc[k].to_list())
+# 		flair_compiled += line + '\n'
+
+# with open('D:\\MCGDYY\\ont_project\\ont_vs_ngs\\assem\\flair_out_compiled.gtf', 'w') as w:
+# 	w.write(flair_compiled)
+
+# convert gtf to a list of dicts
+
+def convert_gtf(gtf_path, tool):
 
 	trans_list = []
 	trans_dict = {}
@@ -23,11 +48,23 @@ def convert_gtf(gtf_path):
 					trans_start = int(line.split('\t')[3])
 					trans_end = int(line.split('\t')[4])
 
-					if 'cmp_ref' in line:	# for scallop whose transcript id is in cmp_ref
-						trans_id = re.findall(r'cmp_ref "(.*?)";', line)[0]
-					else:
-						trans_id = re.findall(r'transcript_id "(.*?)";', line)[0]
-					trans_dict = {'trans_id': trans_id, 'chrom': chrom, 'trans_start': trans_start, 'trans_end': trans_end}
+					if tool == 'gffcompare':
+						trans_type = re.findall(r'class_code "(.*?)";', line)[0]
+						if trans_type == '=':
+							trans_type = 'annotated'
+							trans_id = re.findall(r'cmp_ref "(.*?)";', line)[0]
+						# intergenic
+						elif trans_type == 'u':
+							trans_type = 'intergenic'
+							trans_id = 'intergenic'
+						else:
+							trans_type = 'novel_genic'
+							trans_id = re.findall(r'cmp_ref "(.*?)";', line)[0]
+					if tool == 'flair':
+						trans_id = line.split('\t')[-1]
+						trans_type = 'flair'
+
+					trans_dict = {'trans_type': trans_type, 'trans_id': trans_id, 'chrom': chrom, 'trans_start': trans_start, 'trans_end': trans_end}
 					ex_count = 0
 
 				if entry_type == 'exon':
@@ -37,49 +74,57 @@ def convert_gtf(gtf_path):
 					trans_dict['ex_start' + str(ex_count)] = ex_start
 					trans_dict['ex_end' + str(ex_count)] = ex_end
 
-	# the first dict is empty
+	# the first dict in the list is empty
 	trans_list.pop(0)
 	return trans_list
 
+def compare(in_gtf_a, in_gtf_b, bp_diff = 5):
 
-in_gtf_a = convert_gtf('D:\\MCGDYY\\ont_project\\test\\sclp.sub.gtf')
-print('list a done')
-in_gtf_b = convert_gtf('D:\\MCGDYY\\ont_project\\test\\st.sub.gtf')
-print('list b done')
-intersec = []
+	# retain only transcripts from a
+	intersec_one = []
+	# retain transcript info from both a and b
+	intersec_both = []
 
-for entry_a in in_gtf_a:
-	len_a = len(entry_a)
-	chrom_a = entry_a['chrom']
-	trans_start_a = entry_a['trans_start']
-	trans_end_a = entry_a['trans_end']
-	tried = False
-	
-	for entry_b in in_gtf_b:
-		# try to broadly match two entries
-		if chrom_a == entry_b['chrom']:
-			tried = True			
-			if len_a == len(entry_b):
-				# try to fine match two entries, two entries less than 10 bp are the same transcript
-				for i in range(3, len_a):
-					diff = abs(list(entry_a.values())[i] - list(entry_b.values())[i])
-					if diff <= 10:
-						go = True
-					else:
-						go = False
-						break
-				# found the match!
-				if go:
-					intersec.append(str(entry_a) + str(entry_b))
-					in_gtf_a.remove(entry_a)
-					in_gtf_b.remove(entry_b)
-					break
+	for entry_a in in_gtf_a:
+		len_a = len(entry_a)
+		chrom_a = entry_a['chrom']
+		trans_start_a = entry_a['trans_start']
+		trans_end_a = entry_a['trans_end']
+		tried = False
 		
-		# if you have tried within the same chromosome, and then the new chromosomes are different, 
-		# means you have passed the current chromosome, so no match			
-		else:
-			if tried:
-				break
+		for entry_b in in_gtf_b:
+			# try to broadly match two entries
+			if chrom_a == entry_b['chrom']:
+				tried = True			
+				if len_a == len(entry_b):
+					# try to fine match two entries, two entries less than 10 bp are the same transcript
+					for i in range(5, len_a):
+						diff = abs(list(entry_a.values())[i] - list(entry_b.values())[i])
+						if diff <= bp_diff:	# base pair difference default is 5 bp
+							go = True
+						else:
+							go = False
+							break
+					# found the match!
+					if go:
+						intersec_one.append(entry_a)
+						intersec_both.append(str(entry_a) + str(entry_b))
+						in_gtf_a.remove(entry_a)
+						in_gtf_b.remove(entry_b)
+						break
+			
+			# if you have tried within the same chromosome, and then the new chromosomes are different, 
+			# means you have passed the current chromosome, so no match			
+			else:
+				if tried:
+					break
 
-unique_a = in_gtf_a
-unique_b = in_gtf_b
+	return intersec_one, intersec_both, in_gtf_a, in_gtf_b
+
+
+in_gtf_a = convert_gtf('D:\\MCGDYY\\ont_project\\test\\sclp.sub.gtf', 'gffcompare')
+print('list a done')
+in_gtf_b = convert_gtf('D:\\MCGDYY\\ont_project\\test\\st.sub.gtf', 'gffcompare')
+print('list b done')
+intersec_one, intersec_both, in_gtf_a, in_gtf_b = compare(in_gtf_a, in_gtf_b)
+
