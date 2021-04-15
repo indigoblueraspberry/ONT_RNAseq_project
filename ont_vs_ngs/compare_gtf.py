80f860afe53b5d12ec0a8a02a8ed8a46a9fed836
Yuanchang Fang <robinycfang@gmail.com>
2021-04-15 11:53:31 +0800

    debug

--- ont_vs_ngs/compare_gtf.py
+++ ont_vs_ngs/compare_gtf.py
@@ -74,6 +74,8 @@ def convert_gtf(gtf_path):
 
 def compare(in_gtf_a, in_gtf_b, bp_diff = 5):
 
+	# create a copy of list a for modification later, otherwise modification during a for loop will cause a problem
+	in_gtf_a_cp = in_gtf_a.copy()
 	# retain only transcripts from a
 	intersec_one = []
 	# retain transcript info from both a and b
@@ -96,10 +98,11 @@ def compare(in_gtf_a, in_gtf_b, bp_diff = 5):
 				if len_a == len(entry_b):
 					# try to fine match two entries
 					# two INTRON chains less than diff bp are the same transcript
-					# two free ends of terminal exons less than 100 bp are considered the same, also for single-exon
+					# two free ends of terminal exons less than 100 bp are considered the same, this also works for single-exon transcript
 					go = True
 					for i in range(5, len_a):
 						diff = abs(list(entry_a.values())[i] - list(entry_b.values())[i])
+						print(diff)
 						if i == 5 and diff <= 100:
 							go = True
 						elif 5 < i < len_a - 1 and diff <= bp_diff:	# base pair difference default is 5 bp
@@ -113,8 +116,8 @@ def compare(in_gtf_a, in_gtf_b, bp_diff = 5):
 					if go:
 						intersec_one.append(entry_a)
 						intersec_both.append(str(entry_a) + str(entry_b))
-						in_gtf_a.remove(entry_a)
-						in_gtf_b.remove(entry_b)
+						in_gtf_a_cp.remove(entry_a)
+						in_gtf_b.remove(entry_b)	# the for loop having list b will break anyway, so no need to use a copy of list b
 						break
 			
 			# if you have tried within the same chromosome, and then the new chromosomes are different, 
@@ -130,7 +133,7 @@ def compare(in_gtf_a, in_gtf_b, bp_diff = 5):
 			 counter_list.append(percent)
 			 print(percent)
 
-	return intersec_one, intersec_both, in_gtf_a, in_gtf_b
+	return intersec_one, intersec_both, in_gtf_a_cp, in_gtf_b
 
 def write_it_out(in_dict, out_path):
 
